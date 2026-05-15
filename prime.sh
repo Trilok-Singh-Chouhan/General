ba0da505a5d76c3987168856263fc2b850397289
Trilok Singh Chouhan <tricho7109@gmail.com>
2026-05-15 08:42:29 +0530

    Create prime.sh for prime number checking
    Add a script to check if a number is prime.

---
+++ prime.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Enter number:"
+read n
+
+flag=0
+
+for ((i=2;i<n;i++))
+do
+    if [ $((n%i)) -eq 0 ]
+    then
+        flag=1
+    fi
+done
+
+if [ $flag -eq 0 ]
+then
+    echo "Prime"
+else
+    echo "Not Prime"
+fi
